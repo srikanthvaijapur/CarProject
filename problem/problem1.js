be36e628e7c99fd23b2abef27ee6cdd689118d77
Srikanthvaijapur <srikantvaijapur@gmail.com>
2021-05-11 14:33:40 +0530

    problem1solution

---
+++ problem/problem1.js
@@ -0,0 +1,9 @@
+function problem1(inventory){
+    for(let i = 0 ;i < inventory.length;i++){
+        if(inventory[i].id==33){
+            return `Car 33 is a ${inventory[i].car_year} ${inventory[i].car_make} ${inventory[i].car_model}`;
+        }
+    }
+}
+
+module.exports = problem1 
---
+++ test/testProblem1.js
@@ -0,0 +1,6 @@
+const inventory = require("../inventory");
+const problem1 = require("../problem/problem1");
+
+const result = problem1(inventory);
+// console.log(inventory)
+console.log(result)
