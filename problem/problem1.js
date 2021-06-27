3cad368991da0ee4f2581147a4ca2f2516fc7a63
srikanthvaijapur <srikantvaijapur@gmail.com>
2021-06-27 17:58:14 +0530

    updated problems

--- problem/problem1.js
+++ problem/problem1.js
@@ -1,4 +1,4 @@
-function problem1(inventory){
+function problem(inventory){
     for(let i = 0 ;i < inventory.length;i++){
         if(inventory[i].id==33){
             return `Car 33 is a ${inventory[i].car_year} ${inventory[i].car_make} ${inventory[i].car_model}`;
@@ -6,4 +6,4 @@ function problem1(inventory){
     }
 }
 
-module.exports = problem1 
+module.exports = problem
